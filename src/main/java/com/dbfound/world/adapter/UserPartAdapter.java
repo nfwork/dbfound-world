346de666beb9a6e0a087c44a30237760983fa657
huangjiong <huangjiong@gomo.com>
2023-08-07 15:55:36 +0800

    添加sqlPart例子

---
+++ src/main/java/com/dbfound/world/adapter/UserPartAdapter.java
@@ -0,0 +1,26 @@
+package com.dbfound.world.adapter;
+
+import com.dbfound.world.enums.UserField;
+import com.nfwork.dbfound.core.Context;
+import com.nfwork.dbfound.model.adapter.QueryAdapter;
+import com.nfwork.dbfound.model.bean.Param;
+import com.nfwork.dbfound.model.enums.EnumHandlerFactory;
+import com.nfwork.dbfound.model.enums.EnumTypeHandler;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Component
+public class UserPartAdapter implements QueryAdapter<Map<String,Object>> {
+
+    @Override
+    public void beforeQuery(Context context, Map<String, Param> params) {
+        List<?> fields = (List<?>) context.getData("param.fields");
+        EnumTypeHandler<?> handler = EnumHandlerFactory.getEnumHandler(UserField.class);
+        fields = fields.stream().map(v->handler.locateEnum((String) v)).filter(Objects::nonNull).collect(Collectors.toList());
+        context.setParamData("fields",fields);
+    }
+}
---
+++ src/main/java/com/dbfound/world/enums/UserField.java
@@ -0,0 +1,9 @@
+package com.dbfound.world.enums;
+
+public enum UserField {
+
+    user_code,
+    user_name,
+    role_id,
+    password
+}
