548a44f7fb9521c3d9ac59037cc1284ae020c988
huangjiong <huangjiong@gomo.com>
2023-12-22 11:47:38 +0800

    优化代码

--- src/main/java/com/dbfound/world/DBFoundApp.java
+++ src/main/java/com/dbfound/world/DBFoundApp.java
@@ -2,9 +2,8 @@ package com.dbfound.world;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class DBFoundApp
 {
     public static void main( String[] args )
